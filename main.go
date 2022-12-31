c8d594f4f4e84c70d203a7d1f1f2d824343e7bfb
wikla pandu <wiklapandu2503@gmail.com>
2022-12-31 14:36:17 +0700

    feat: create model blog, user and helper

--- main.go
+++ main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+
+	blogModel "go-restapi/models/blog"
+	userModel "go-restapi/models/user"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
+type MyClaims struct {
+	*jwt.StandardClaims
+	ID    uint   `json:"id" form:"id"`
+	Name  string `json:"name" form:"name"`
+	Email string `json:"email" form:"email"`
+}
+
+var SecretKey = []byte(os.Getenv("SECRET_KEY"))
+
+var DB *gorm.DB
+
 func main() {
+	var err = godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
+
+	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ")/" + os.Getenv("DB_NAME") + "?charset=utf8mb4&parseTime=True&loc=Local"
+	database, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	database.AutoMigrate(&blogModel.Blog{}, &blogModel.Category{}, &blogModel.BlogCats{})
+	database.AutoMigrate(&userModel.User{})
+
+	DB = database
+
 	route := gin.Default()
 
 	config := cors.DefaultConfig()
@@ -15,4 +50,60 @@ func main() {
 	config.AllowCredentials = true
 
 	route.Use(cors.New(config))
+
+	route.POST("/register", func(ctx *gin.Context) {
+		var inputPost struct {
+			Name           string `json:"name" form:"name" binding:"required"`
+			Email          string `json:"email" form:"email" binding:"required,email"`
+			Password       string `json:"password" form:"password" binding:"required"`
+			RepeatPassword string `json:"repeat_password" form:"repeat_password" binding:"required"`
+		}
+
+		if err := ctx.Bind(&inputPost); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"status": "errors",
+				"errors": err.Error(),
+			})
+
+			ctx.Abort()
+			return
+		}
+
+		if inputPost.Password != inputPost.RepeatPassword {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"status":  "fail",
+				"message": "password not matches with repeat password",
+			})
+
+			ctx.Abort()
+			return
+		}
+
+		user := userModel.User{Name: inputPost.Name, Email: inputPost.Email}
+
+		sign := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), MyClaims{
+			ID:    123,
+			Name:  inputPost.Name,
+			Email: inputPost.Email,
+		})
+
+		token, err := sign.SignedString(SecretKey)
+
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"status":  "fail",
+				"message": "unauthorized token",
+			})
+
+			ctx.Abort()
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": "success",
+			"token":  token,
+		})
+	})
+
+	route.Run()
 }
---
+++ models/blog/blog.go
@@ -0,0 +1,26 @@
+package blog
+
+import "gorm.io/gorm"
+
+type Blog struct {
+	*gorm.Model
+	Title  string `json:"title" gorm:"column:title"`
+	Desc   string `json:"desc" gorm:"column:desc"`
+	Author uint   `json:"user_id" gorm:"column:user_id"`
+}
+
+type CatMeta struct {
+	Desc string `json:"desc"`
+}
+
+type Category struct {
+	*gorm.Model
+	Name     string  `json:"name" gorm:"column:name"`
+	MetaData CatMeta `json:"metadata" gorm:"embedded;column:metadata;type:longtext"`
+}
+
+type BlogCats struct {
+	*gorm.Model
+	Blog     uint `json:"blog_id" gorm:"column:blog_id"`
+	Category uint `json:"cat_id" gorm:"column:cat_id"`
+}
